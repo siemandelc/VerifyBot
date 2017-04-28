f97d551756e7e90f81dc0ad3125390e9ed893255
Calvin Siemandel <calvin.siemandel@gmail.com>
2017-04-27 19:22:01 -0500

    More error handling.

--- src/VerifyBot/Services/StatisticsService.cs
+++ src/VerifyBot/Services/StatisticsService.cs
@@ -19,54 +19,61 @@ namespace VerifyBot.Services
 
         public async Task GetStatistics()
         {
-            Console.WriteLine("Calculating Statistics");
-
-            var counts = new Dictionary<int, int>();
-            var discordUsers = await manager.GetDiscordUsers();
-
-            var timestamp = DateTime.Now;
-            var count = 0;
-
-            foreach (var discordUser in discordUsers)
+            try
             {
-                var dbUser = await manager.GetDatabaseUser(discordUser.Id);
+                Console.WriteLine("Calculating Statistics");
 
-                if (dbUser == null)
+                var counts = new Dictionary<int, int>();
+                var discordUsers = await manager.GetDiscordUsers();
+
+                var timestamp = DateTime.Now;
+                var count = 0;
+
+                foreach (var discordUser in discordUsers)
                 {
-                    continue;
-                }
-                try
-                {
-                    var verifier = VerifyService.Create(dbUser.AccountID, dbUser.APIKey, manager, discordUser, strings);
+                    var dbUser = await manager.GetDatabaseUser(discordUser.Id);
 
-                    await verifier.LoadAccount();
-
-                    if (!counts.ContainsKey(verifier.World))
+                    if (dbUser == null)
                     {
-                        counts.Add(verifier.World, 0);
+                        continue;
+                    }
+                    try
+                    {
+                        var verifier = VerifyService.Create(dbUser.AccountID, dbUser.APIKey, manager, discordUser, strings);
+
+                        await verifier.LoadAccount();
+
+                        if (!counts.ContainsKey(verifier.World))
+                        {
+                            counts.Add(verifier.World, 0);
+                        }
+
+                        counts[verifier.World] = counts[verifier.World]++;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine($"Could not load information for user {discordUser.Nickname ?? discordUser.Username} ({dbUser.APIKey})");
                     }
 
-                    counts[verifier.World] = counts[verifier.World]++;
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine($"Could not load information for user {discordUser.Nickname ?? discordUser.Username} ({dbUser.APIKey})");
+                    count++;
+
+                    if (count >= 450 && (DateTime.Now - timestamp).Seconds < 60)
+                    {
+                        Console.WriteLine("Rate Limited, waiting");
+                        await Task.Delay((DateTime.Now - timestamp).Seconds + 1);
+                        timestamp = DateTime.Now;
+                        Console.WriteLine("Resuming stats");
+                    }
                 }
 
-                count++;
-
-                if (count >= 450 && (DateTime.Now - timestamp).Seconds < 60)
+                foreach (var value in counts)
                 {
-                    Console.WriteLine("Rate Limited, waiting");
-                    await Task.Delay((DateTime.Now - timestamp).Seconds + 1);
-                    timestamp = DateTime.Now;
-                    Console.WriteLine("Resuming stats");
+                    Console.WriteLine($"World [{value.Key}]: {value.Value}");
                 }
             }
-
-            foreach (var value in counts)
+            catch (Exception ex)
             {
-                Console.WriteLine($"World [{value.Key}]: {value.Value}");
+                Console.WriteLine($"Exception: {ex.Message}");
             }
         }
     }
